59de8654e47d54e57da1c4f9f5aa6d5205ad6949
realzyryan <Znp22487@gmail.com>
2024-05-28 23:21:39 +0500

    src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+
+        // int ticketPrice = 13_676;
+        int ticketPrice = 50_266;
+        int rublesPerMile = 20;
+        int resualt = (ticketPrice / rublesPerMile);
+        System.out.println(resualt);
+
+
+    }
+}
